513df854c643239fa8a842ce0a88aa34c92c5af0
manatunga <janithamanatunga@gmail.com>
2026-09-13 21:12:41 +0530

    feat: add os detection layer for repopy

---
+++ src/repopy/os_detector.py
@@ -0,0 +1,26 @@
+'''
+OS Detection layer. Contains the importable logic required to
+find the local machine's underlying operating system.
+'''
+
+import platform
+
+def get_os() -> str:
+
+    system = platform.system()
+
+    if system == 'Windows':
+        return 'windows'
+    if system == 'Darwin':
+        return 'mac'
+
+    return 'linux'
+
+def is_windows() -> bool:
+    return get_os() == 'windows'
+
+def is_mac() -> bool:
+    return get_os() == 'mac'
+
+def is_linux() -> bool:
+    return get_os() == 'linux'
